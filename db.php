1faebf61b1089e11d11e794f9bf5b0b1f7a94ee0
Mohamed Bensaleh <mobensaleh@outlook.com>
2021-01-20 01:58:44 -0600

    Added values for the database

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
 
-$server = "";
-$username = "";
-$password = "";
-$dbname="";
+$server = "sql300.epizy.com";
+$username = "epiz_27731947";
+$password = "hJifnpe8AZ3qNBU";
+$dbname="epiz_27731947_database";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
 if(!$conn){
     die("Connection Failed:".mysqli_connect_error());
 }
+
+?>
